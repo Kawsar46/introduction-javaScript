f0f09722f206575d006f1fc3eba00a306fd0e125
kawsar46 <you@example.com>
2023-01-25 21:31:33 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+// vary+able=variable
+//numeric
+var price = 15;
+var age = 20;
+var temperature = 34;
+var weight = 60;
+var money = 100;
+var height = 170;
+//string
+var name = "kawsar ahmed";
+var location = "kakrail";
+//boolean
+var isFullMarks = true;
+var serious = true;
